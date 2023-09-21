510c4e9b9dbd2693ddda433a597c5f1750f1d430
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-09-21 23:47:00 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import random
 
-st.title("おみくじアプリ")
+st.title("テスト勉強")
 
-if st.button("おみくじを引く"):
-    results = ["大吉","中吉","小吉","吉","凶","大凶"]
+if st.button("今日勉強すべき教科"):
+    results = ["国語","英語","数学","数学","化学","物理","地理","公共","公共","公共","公共","公共","公共","公共","公共"]
     result = random.choice(results)
-    st. write(f"結果:｛result｝")
+    st.write(f"結果:｛result｝")
