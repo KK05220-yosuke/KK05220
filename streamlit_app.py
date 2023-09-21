1b747c43340816a956743fae5285de7962896a21
KK05220-yosuke <144981172+KK05220-yosuke@users.noreply.github.com>
2023-09-21 23:49:16 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import random
 st.title("テスト勉強")
 
 if st.button("今日勉強すべき教科"):
+    
     results = ["国語","英語","数学","数学","化学","物理","地理","公共","公共","公共","公共","公共","公共","公共","公共"]
     result = random.choice(results)
     st.write(f"結果:｛result｝")
